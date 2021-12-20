9ef655a00289602ded5010847eb254809366e81a
2123025 <lsi371@naver.com>
2021-12-20 21:32:41 +0900

    Make square.js
    circle.js를 복제 및 변형하여 square.js를 추가

---
+++ square.js
@@ -0,0 +1,52 @@
+class Square {
+
+  constructor(x, y, size){
+    this.makeBody(x, y, size);
+  }
+
+  makeBody(x, y, size) {
+
+    this.x = x;
+    this.y = y;
+    this.size = size;
+
+    let bd = new box2d.b2BodyDef();
+    bd.type = box2d.b2BodyType.b2_dynamicBody;
+    bd.position = scaleToWorld(x, y);
+
+    // Define fixture #1
+    let fd = new box2d.b2FixtureDef();
+    // Fixture holds shape
+    fd.shape = new box2d.b2PolygonShape();
+    fd.shape.SetAsBox(scaleToWorld(this.size / 2), scaleToWorld(this.size / 2));
+    fd.density = 1.0;
+    fd.friction = 0.5;
+    fd.restitution = 0.2;
+
+    // Create the body
+    this.body = world.CreateBody(bd);
+    // Attach the fixture
+    this.body.CreateFixture(fd);
+
+    // Some additional stuff
+    this.body.SetLinearVelocity(new box2d.b2Vec2(random(-5, 5), random(2, 5)));
+    this.body.SetAngularVelocity(random(-3, 3));
+  }
+
+  display() {
+    let pos = scaleToPixels(this.body.GetPosition());
+    // Get its angle of rotation
+    let a = this.body.GetAngleRadians();
+
+    // Draw it!
+    rectMode(CENTER);
+    push();
+    translate(pos.x, pos.y);
+    rotate(a);
+    fill(200);
+    noStroke();
+
+    rect(0, 0, this.size, this.size);
+    pop();
+  }
+}
