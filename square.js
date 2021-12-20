390747f72f9462ce337015cf25b168519c3d590f
2123025 <lsi371@naver.com>
2021-12-20 21:43:49 +0900

    Resize and give offset to square.js
    square 크기 조정, 간격 추가

--- square.js
+++ square.js
@@ -14,39 +14,32 @@ class Square {
     bd.type = box2d.b2BodyType.b2_dynamicBody;
     bd.position = scaleToWorld(x, y);
 
-    // Define fixture #1
     let fd = new box2d.b2FixtureDef();
-    // Fixture holds shape
     fd.shape = new box2d.b2PolygonShape();
-    fd.shape.SetAsBox(scaleToWorld(this.size / 2), scaleToWorld(this.size / 2));
+    fd.shape.SetAsBox(scaleToWorld(this.size), scaleToWorld(this.size));
     fd.density = 1.0;
     fd.friction = 0.5;
     fd.restitution = 0.2;
 
-    // Create the body
     this.body = world.CreateBody(bd);
-    // Attach the fixture
     this.body.CreateFixture(fd);
 
-    // Some additional stuff
     this.body.SetLinearVelocity(new box2d.b2Vec2(random(-5, 5), random(2, 5)));
     this.body.SetAngularVelocity(random(-3, 3));
   }
 
   display() {
     let pos = scaleToPixels(this.body.GetPosition());
-    // Get its angle of rotation
     let a = this.body.GetAngleRadians();
 
-    // Draw it!
     rectMode(CENTER);
     push();
     translate(pos.x, pos.y);
     rotate(a);
-    fill(200);
+    fill(220);
     noStroke();
 
-    rect(0, 0, this.size, this.size);
+    rect(0, 0, this.size * 2 - 3 , this.size * 2 - 3);
     pop();
   }
 }
